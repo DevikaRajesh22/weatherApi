11a5915b5f92ccb50a8efbb8d0144be08150ea7b
DevikaRajesh22 <devikaraj699@gmail.com>
2024-03-06 15:49:57 +0530

    first api for geolocation

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,10 +1,21 @@
 import { ChangeEvent, useState } from "react";
+import { optionType } from "./types";
 
 const App = (): JSX.Element => {
   const [term, setTerm] = useState<string>('');
+  const [options, setOptions] = useState<[]>([]);
+  const getSearchOptions = (value: string) => {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${process.env.REACT_APP_API_KEY}`).then(res => res.json()).then((data) => setOptions(data));
+  }
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTerm(e.target.value);
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=${limit}&appid=${API key}`);
+    const value = e.target.value.trim();
+    setTerm(value);
+    if (value === '') return;
+    getSearchOptions(value);
+  }
+  const onOptionSelect=(option:optionType)=>{
+    console.log(option);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${option.lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`).then(res=>res.json()).then(data=>console.log(data));
   }
   return (
     <main className="flex justify-center items-center bg-darkestPurple h-[100vh] w-full">
@@ -16,13 +27,20 @@ const App = (): JSX.Element => {
           Enter below the place you want to know the weather of and select an
           option from the dropdown
         </p>
-        <div className="flex mt-10 md:mt-4">
+        <div className="relative flex mt-10 md:mt-4">
           <input
             type="text"
             value={term}
             className="px-2 py-1 rounded-1-md border-2 border-white"
             onChange={onInputChange}
           />
+          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
+            {options.map((option: optionType, index: number) => (
+              <li key={option.name + '-' + index}>
+                <button className="text-left text-sm w-full hover:bg-zinc-700 hover:text-white px-2 py-1 cursor-pointer" onClick={()=>onOptionSelect(option)}>{option.name}</button>
+              </li>
+            ))}
+          </ul>
           <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
             Search
           </button>
