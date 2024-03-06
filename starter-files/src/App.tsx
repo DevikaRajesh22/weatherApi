8f30b7ef7b53a3456497c6fa6efe1eb942ce7503
DevikaRajesh22 <devikaraj699@gmail.com>
2024-03-06 11:32:50 +0530

    search ui

---
+++ starter-files/src/App.tsx
@@ -0,0 +1,27 @@
+const App = () => {
+  return (
+    <main className="flex justify-center items-center bg-darkestPurple h-[100vh] w-full">
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+        <h1 className="text-white text-4xl font-thin">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+        <p className="text-white text-sm mt-2">
+          Enter below the place you want to know the weather of and select an
+          option from the dropdown
+        </p>
+        <div className="flex mt-10 md:mt-4">
+          <input
+            type="text"
+            value={''}
+            className="px-2 py-1 rounded-1-md border-2 border-white"
+          />
+          <button className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer">
+            Search
+          </button>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ starter-files/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,ts,tsx}'],
+  theme: {
+    extend: {
+      colors:{
+        darkestPurple: '#2a2251',
+      },
+    },
+  },
+  plugins: [],
+}
